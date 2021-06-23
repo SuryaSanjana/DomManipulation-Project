89a011cdb5e7a3dc83e3349c82110d9ca9590510
SuryaSanjana <sanjanaailavarapu26@gmail.com>
2021-06-23 12:10:28 +0530

    fix modal close

--- index.js
+++ index.js
@@ -2,48 +2,69 @@
 const taskContainer=document.querySelector(".task__container");
 console.log(taskContainer);
 
-const newCard=(
-    {
-    id,
-    imageUrl,
-    taskDAta,
-    taskType,
-    taskDescription,
-})=>`<div class="col-md-6 col-lg-4">
-<div class="card text-center">
-  <div class="card-header  d-flex justify-content-end gap-2">
-    <button type="button" class="btn btn-outline-success">
-      <i class="fas fa-pencil-alt"></i>
-    </button>
-    <button type="button" class="btn btn-outline-danger">
-      <i class="fas fa-dumpster"></i>
-    </button>
-  </div>
-  <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQlF12944TXKLlGMzXn6DQGu1N-yt6UE8vs4g&usqp=CAU"
-   class="card-img-top" alt="..."
-  />
-  <div class="card-body">
-    <h5 class="card-title">Special title treatment</h5>
-    <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
-    <span class="badge bg-primary">Amazing</span>
-  </div>
-  <div class="card-footer text-muted">
-    <button type="button" class="btn btn-outline-primary float-end rounded-pill">
-      Open Task</button>
-  </div>
-</div>
-</div>`
+//global store
+//const globalStore=[];
 
+// const loadInitialTaskCards=() => {
+//   //access local storage
+//  const getInitialData=localStorage.tasky;
+//  if(!getInitialData)  return;
+//   // convert strinified-object to object
+//   const taskObjects =JSON.parse(getInitialData);
+//   //map around the array to generate html cards and inject it to DOM
+//   cards.map((cardObject) =>{
+//     const createNewCard=newCard(cardObject);
+//     taskContainer.insertAdjacentHTML("beforeend",createNewCard); //to insert inside parent element
+//   globalStore.push(cardObject);
+//   });
+// };
+
+const newCard=(
+  {
+  id,
+  imageUrl,
+  taskTitle,
+  taskType,
+  taskDescription,
+})=>`<div class="col-md-6 col-lg-4 id=${id}">
+<div class="card text-center">
+<div class="card-header  d-flex justify-content-end gap-2">
+  <button type="button" class="btn btn-outline-success">
+    <i class="fas fa-pencil-alt"></i>
+  </button>
+  <button type="button" class="btn btn-outline-danger">
+    <i class="fas fa-dumpster"></i>
+  </button>
+</div>
+<img src=${imageUrl}
+ class="card-img-top" alt="..."
+/>
+<div class="card-body">
+  <h5 class="card-title">${taskTitle}</h5>
+  <p class="card-text">${taskDescription}</p>
+  <span class="badge bg-primary">${taskType}</span>
+</div>
+<div class="card-footer text-muted">
+  <button type="button" class="btn btn-outline-primary float-end rounded-pill">
+    Open Task</button>
+</div>
+</div>
+</div>`;
 
 const saveChanges = ()=> {
-    const taskDAta ={
-        id: `${Date.now()}`,  //unique number for card id
-        imageUrl :document.getElementById("imageUrl").value,
-        taskTitle:document.getElementById("tasktitle").value,
-        taskType: document.getElementById("tasktype").value,
-        taskDescription: document.getElementById("taskdescription").value,
-    };
-
-    const createNewCard = newCard(taskData);
-    taskContainer.insertAdjacentHTML("beforeend")
+ const taskData = {
+   id: `${Date.now()}`,  //unique number for card id
+   imageUrl :document.getElementById("imageurl").value,
+   taskTitle:document.getElementById("tasktitle").value,
+   taskType: document.getElementById("tasktype").value,
+   taskDescription: document.getElementById("taskdescription").value,
+ };
+ 
+ // HTML Code
+ const createNewCard = newCard(taskData);
+  taskContainer.insertAdjacentHTML("beforeend",createNewCard); //to insert inside parent element
+  // globalStore.push(taskData);
+  // //Application Programming Interface(API)
+  // //adding to local storage
+  // localStorage.setItem("tasky",JSON.stringify({cards: globalStore}));    
 };
